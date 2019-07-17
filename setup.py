805c8118ec2d74db4cc309ee8d321695d69784ae
kyle-verhoog <kyle@verhoog.ca>
2019-07-16 22:09:35 -0400

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ from setuptools import find_packages, setup
 
 
 def get_version(package):
-    """
-    Return package version as listed in `__version__` in `__init__.py`.
-    This method prevents to import packages at setup-time.
-    """
     init_py = open(os.path.join(package, '__init__.py')).read()
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
@@ -16,16 +12,22 @@ version = get_version('nem')
 
 
 setup(
+    name='nem',
+    version=version,
+    description='mnemonics for your terminal',
+    url='https://github.com/kyle-verhoog/nem',
+    long_description='mnemonics for your terminal.',
     entry_points={
         'console_scripts': [
             'n = nem.nem:nem',
         ],
     },
-    description='mnemonics for your terminal',
-    long_description='mnemonics for your terminal.',
     long_description_content_type='text/markdown',
-    name='nem',
     packages=find_packages(exclude=['tests*']),
-    url='https://github.com/kyle-verhoog/nem',
-    version=version,
+    install_requires=[
+        'colouredlogs',
+        'prompt-toolkit',
+        'tabulate',
+        'toml',
+    ],
 )
